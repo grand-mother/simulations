dc29fa22e5c96fbbf96c94affd45752c7b79d4f5
zhang jianli <jlzhang@ybjslc02.ihep.ac.cn>
2018-01-18 14:37:57 +0800

    add if loop to check voltage files

--- computevoltage_Xmax.sh
+++ computevoltage_Xmax.sh
@@ -11,6 +11,9 @@ myfields=$basedir/$pool/fields
 myvoltages=$basedir/$pool/voltages
 myseeds=$basedir/$pool/seeds
 
+#$todofile=$basedir/computevoltage_Xmax.sh.todofile
+#ntotal=`cat $todofile |wc -l`
+
 #donefile=${0}.done
 donefile=$basedir/computevoltage_Xmax.sh.done
 echo $donefile
@@ -22,10 +25,13 @@ if [ -f $donefile ] ; then
 
    dirs=`find $myfields/ -name "*.tgz" -type f  -newer ${lastfieldid}.tgz |xargs ls -tr` #jlzhang, when rerun, avoid the time order bug   
    ndirs=`find $myfields/ -name "*.tgz" -type f  -newer ${lastfieldid}.tgz | wc -l`
+   #lastfieldidn=`grep -n $lastfieldid $todofile  | gawk -F ':' '{print $1}` #if all field files are ready
+   #dirs=`tail -$lastfieldidn $todofile`
+   #ndirs=$((ntotal-n))
 else
-   #dirs=`find $myfields/ -name "*.tgz" |xargs ls -tr`
-   #ndirs=`find $myfields/ -name "*.tgz" |wc -l`
-   dirs=/sps/trend/jlzhang/production/grandproto/fields/10850122/out.tar.gz #for test only
+   dirs=`find $myfields/ -name "*.tgz" |xargs ls -tr`
+   ndirs=`find $myfields/ -name "*.tgz" |wc -l`
+   #dirs=/sps/trend/jlzhang/production/grandproto/fields/10850122/out.tar.gz #for test only
 fi
 echo "Nb of fields to be processed: $ndirs" 
 if [ $ndirs -eq 0 ] ;then
@@ -37,6 +43,8 @@ echo "Press a key to proceed..."
 #read touche
 #stty icanon
 
+nout=0
+ntracet=0
 #for dir in  $myfields/E*/*
 for dir in $dirs 
 #for dir in /scratchfs/ybj/jlzhang/grand/sim/flat-100x100km2/fields/E.5e18_X.181841_Y.39925_Z.541_T.91_P.33_D.12135181768782943.tgz
@@ -51,28 +59,42 @@ do
 
   ntracet=`tar ztf $dir |wc -l`
   ntracet=$((ntracet-2))
-  ntrace=`ls $rundir|wc -l`
-  ntrace=$((ntrace-2))
-  nout=`ls $myvoltages/$runname|wc -l`
+  if [ -d $rundir ] ; then  
+     ntrace=`ls $rundir|wc -l`
+     ntrace=$((ntrace-2))
+  fi  
+  #nout=`ls $myvoltages/$runname|wc -l`
   #for satety, we can use the biggst antenna id.
   #FILETEST="$myvoltages/$runname/doing.txt"
+  FILETEST="$myvoltages/$runname"
   #echo $FILETEST
   #for safety, we can check whether the no. of trace and out files are equal
   #if [ $ntracet = $nout -a -f  "$FILETEST" -a ! -s "$FILETEST" ] ; then
   #if [ $ntracet = $nout -a -s $myvoltages/$runname.tgz ] ; then
   #if [ -f $FILETEST ] ; then
-  #   echo "Field is being processed: $myvoltages/$runname/doing.txt exists"
-  #fi
+  #this if loop and next, this loop is true, but there may be no voltages files!!! this if loop avoid multi process doing the same run. so for the check script, comment the continue in this loop.
+  if [ -d $FILETEST ] ; then
+     nout=`ls $myvoltages/$runname|wc -l`
+     echo "Field is being processed: $myvoltages/$runname exists"
+     continue
+  elif [ -f $FILETEST.tgz ]; then
+       nout=`tar ztf $$FILETEST.tgz |wc -l`
+       nout=$((nout-2))
+       echo "Field is being processed: $myvoltages/$runname.tgz exists"
+       continue
+  fi
 
   if [ $ntracet = $nout ] ; then #note the blank space before and after "="  
      echo "Field was already processed:  $ntracet==$nout, file $myvoltages/$runname exists!"
      echo $rundir >>  $donefile
     #echo  "Writing fieldID to donefile."
     #echo $fieldid  >> $wrkdir/donefile
-    continue
+     continue
   fi
 
-  #mkdir -p 
+  mkdir -p $myvoltages/$runname
+  #touch $FILETEST
+
   #if [ $ntracet!=$ntrace -a ! -f $myfields/$runname/a0.trace ] ; then
   if [ $ntracet!=$ntrace  ] ; then
      echo "extract $myfields/${runname}.tgz"
@@ -80,8 +102,6 @@ do
      rm -rf $myfields/${runname}
      tar zxf $myfields/${runname}.tgz  
   fi
-  mkdir -p $myvoltages/$runname
-  #touch $FILETEST
 
   echo "Now computing antenna response for $rundir..."
   thetat=${runname#*T.}
@@ -105,10 +125,9 @@ do
      cd $myvoltages
      rm -rf $myvoltages/$runname.tgz
      tar -czf $myvoltages/$runname.tgz $runname 
-     rm -f $FILETEST
   else
      echo "$wrkdir/computevoltage_Xmax.py error!!!"
-     rm -rf $myvoltages/$runname $myvoltages/$runname.tgz
+     rm -rf $myfields/$runname $myvoltages/$runname $myvoltages/$runname.tgz
      continue 
   fi
 
