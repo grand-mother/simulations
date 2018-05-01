df95c198937ac58d994f6f539bfc77426fdf2a2a
Olivier Martineau <omartino@in2p3.fr>
2018-05-01 23:06:24 +0200

    Now computeAttenuatedResponse takes directories in input

--- computeAttenuatedResponse.py
+++ computeAttenuatedResponse.py
@@ -1,5 +1,6 @@
 import sys
 import glob
+import shutil
 import time
 import modules
 import os.path
@@ -9,7 +10,7 @@ import pylab as pl
 from scipy.interpolate import interp1d
 from scipy.fftpack import rfft, irfft, rfftfreq            
 
-CC = 1
+CC = 0
 if CC==1:
   RETRODIR = "/pbs/throng/trend/soft/sim/GRANDsim/retro/"
 else:
@@ -80,66 +81,87 @@ def attenuate(f,attdB):
     return res
 
 
-def process(target):
-
-  print "o Processing", target
-  t0 = time.time()
-
-  # Set target path
-  #evtpath = "/home/martineau/GRAND/GRAND/data/massProd/flat/"
-  evtpath = './'
-  jsonf = evtpath+target+".voltage.json"
-  attf = evtpath+target+".att"
-  tarf =  evtpath+target+".tgz"
-
-  if os.path.isfile(tarf):
-    # Unpack tar file with traces
-    print "Extracting",tarf
-    tar = tarfile.open(tarf, "r:gz")
-    tar.extractall(path=evtpath)
-    tar.close()
-    print "Done."
-  else:
-    print "No file",tarf,"! Abort."
-    return
-    
-  # Load attenuation
-  print "Loading attenuation table",attf
-  if os.path.isfile(attf):
-    attt = np.loadtxt(attf)
-    antid = attt[:,0]
-    att = attt[:,1:]
-  else:
-    print "No file",attf,"! Abort."
-    return
+def process(jsonpath,attpath=None,tarpath=None):
+  untardir = "./tmp"
+  try:
+    os.stat(untardir)
+  except:
+    os.mkdir(untardir)   
   
-  # Now loop on events in json file (Should only be one)
-  for evt in EventIterator(jsonf):  
-    print "Now computing attenuated voltages"
-    voltage=[]
-    time_peaks=[]
-    # Now loop on all antennas
-    for i in range(int(max(antid))):
+  for name in os.listdir(jsonpath):
+    if not name.endswith("json"):
+    	continue
+    jsonf = name
+    target = jsonf[:-13]
+  
+    print "o Processing", target
+    t0 = time.time()
+    if attpath == None:
+      attpath = jsonpath+"/"
+    if tarpath == None:
+      tarpath = jsonpath+"/"
+    jsonf = jsonpath+"/"+jsonf 
+    attf = attpath+target+".att"
+    tarf =  tarpath+target+".tgz"
+
+    if os.path.isfile(tarf):
+      # Unpack tar file with traces
+      print "Extracting",tarf
+      tar = tarfile.open(tarf, "r:gz")
+      tar.extractall(path=untardir)
+      tar.close()
+      print "Done."
+    else:
+      print "No file",tarf,"! Abort."
+      continue
  
-      file_freespace = evtpath+target+"/out_{0}.txt".format(i)
-      if os.path.isfile(file_freespace):  # Voltage was computed
-    	res = attenuate(file_freespace,att[i,:])
-    	v_list = (i,round(res[1]-res[3],3),round(res[5]-res[7],3),round(res[9]-res[11],3),round(res[13]-res[15],3))
-    	voltage.append( v_list )
-    	time_peaks.append(res)
+    # Load attenuation
+    print "Loading attenuation table",attf
+    if os.path.isfile(attf):
+      attt = np.loadtxt(attf)
+      antid = attt[:,0]
+      att = attt[:,1:]
+    else:
+      print "No file",attf,"! Abort."
+      continue
  
-    # Now write output to file
-    print "Now logging results to",jsonf
-    log_event = EventLogger(path=jsonf)
-    # Add the additional informations to the shower event
-    evt['voltage'] = voltage # muV
-    evt['time_peaks'] = time_peaks # s, muV
-    log_event(**evt)
+    # Now loop on events in json file (Should only be one)
+    for evt in EventIterator(jsonf):
+      print "Now computing attenuated voltages"
+      voltage=[]
+      time_peaks=[]
+      # Now loop on all antennas
+      for i in range(int(max(antid))):
+ 
+  	file_freespace = untardir+target+"/out_{0}.txt".format(i)
+  	if os.path.isfile(file_freespace):  # Voltage was computed
+  	  res = attenuate(file_freespace,att[i,:])
+  	  v_list = (i,round(res[1]-res[3],3),round(res[5]-res[7],3),round(res[9]-res[11],3),round(res[13]-res[15],3))
+  	  voltage.append( v_list )
+  	  time_peaks.append(res)
+ 
+      # Now write output to file
+      print "Now logging results to",jsonf
+      log_event = EventLogger(path=jsonf)
+      # Add the additional informations to the shower event
+      evt['voltage'] = voltage # muV
+      evt['time_peaks'] = time_peaks # s, muV
+      log_event(**evt)
 
-  print "  --> Done in {:.1f} s".format(time.time() - t0)
-
-
+    print "  --> Done in {:.1f} s".format(time.time() - t0)
+    shutil.rmtree(untardir)
    
 if __name__ == "__main__":
-    print "Usage: >python computeAttenuatedResponse.py <ID of target event>"
-    process(sys.argv[1])
+    if len(sys.argv)==1:
+    	print "Usage: >python computeAttenuatedResponse.py <path to json> [<path to att>] [<path to tgz>]"
+    else:	
+      print len(sys.argv)
+      attpath, tarpath = None, None
+      if len(sys.argv)>1:
+        jsonpath = sys.argv[1]
+      if len(sys.argv)>2:
+        attpath = sys.argv[2]
+      if len(sys.argv)>3:
+        tarpath = sys.argv[3]
+      
+      process(jsonpath,attpath,tarpath)
